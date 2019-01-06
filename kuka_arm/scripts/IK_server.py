2a323739e7d3f0a8e9b77f426831a42bca4c3b62
Peter Hurt <peter.hurt@web.de>
2019-01-06 04:43:29 -0800

    Added the DH Parameters numerical by using kr210.urdf.xacro robot information.

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -41,6 +41,14 @@ def handle_calculate_IK(req):
 	#
 	# Create Modified DH parameters
 	#
+        s = {alpha0:     0, a0:      0, d1:  0.75,
+             alpha1: -pi/2, a1:   0.35, d2:     0, q2: q2-pi/2,
+             alpha2:     0, a2:   1.25, d3:     0, 
+             alpha3: -pi/2, a3: -0.054, d4:  1.50,
+             alpha4:  pi/2, a4:      0, d5:     0,
+             alpha5: -pi/2, a5:      0, d6:     0,
+             alpha6:     0, a6:      0, d7: 0.303, q7: 0}
+
         #
 	# Define Modified DH Transformation matrix
 	#
