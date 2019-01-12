00e749b5a8de8319c4549c35192ae7d6b6f2ff95
Peter Hurt <peter.hurt@web.de>
2019-01-12 00:45:34 -0800

    EXTENDED: Inserted the definition of homogeneous transforms between each joint. The transformation matrix is derived by the Denavit-Hartenberger Parameters.

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -52,7 +52,48 @@ def handle_calculate_IK(req):
         #
 	# Define Modified DH Transformation matrix
 	#
-	#
+	# LINK 1 (i = 1)
+        T0_1 = Matrix([[            cos(q1),            -sin(q1),            0,              a0]
+                       [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1]
+                       [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1]
+                       [                  0,                   0,            0,               1]
+        T0_1 = T0_1.subs(s)
+        
+        # LINK 2
+        T0_2 = Matrix([[            cos(q2),            -sin(q2),            0,              a1]
+                       [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2]
+                       [sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2]
+                       [                  0,                   0,            0,               1]
+        T0_2 = T0_2.subs(s)
+        
+        # LINK 3
+        T0_3 = Matrix([[            cos(q3),            -sin(q3),            0,              a2]
+                       [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d2]
+                       [sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d2]
+                       [                  0,                   0,            0,               1]
+        T0_3 = T0_3.subs(s)
+        
+        # LINK 4 
+        T0_4 = Matrix([[            cos(q4),            -sin(q4),            0,              a3]
+                       [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4]
+                       [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4]
+                       [                  0,                   0,            0,               1]
+        T0_4 = T0_4.subs(s)
+        
+        # LINK 5
+        T0_5 = Matrix([[            cos(q5),            -sin(q5),            0,              a4]
+                       [sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5]
+                       [sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5]
+                       [                  0,                   0,            0,               1]
+        T0_5 = T0_5.subs(s)
+        
+        # LINK 6
+        T0_6 = Matrix([[            cos(q6),            -sin(q6),            0,              a5]
+                       [sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6]
+                       [sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6]
+                       [                  0,                   0,            0,               1]
+        T0_6 = T0_6.subs(s)
+        	#
 	# Create individual transformation matrices
 	#
 	#
