12b2c29523c78d480814906f7fc6f6d6aec993b2
Peter Hurt <peter.hurt@web.de>
2019-01-13 05:04:39 -0800

    FORWARD KINEMATICS: Implemented the forward kinematics problem.

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -52,54 +52,88 @@ def handle_calculate_IK(req):
         #
 	# Define Modified DH Transformation matrix
 	#
-	# LINK 1 (i = 1)
-        T0_1 = Matrix([[            cos(q1),            -sin(q1),            0,              a0]
-                       [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1]
-                       [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1]
-                       [                  0,                   0,            0,               1]
+	# LINK 0->1 (i = 1)
+        T0_1 = Matrix([[            cos(q1),            -sin(q1),            0,              a0],
+                       [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+                       [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+                       [                  0,                   0,            0,               1]])
         T0_1 = T0_1.subs(s)
         
-        # LINK 2
-        T0_2 = Matrix([[            cos(q2),            -sin(q2),            0,              a1]
-                       [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2]
-                       [sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2]
-                       [                  0,                   0,            0,               1]
-        T0_2 = T0_2.subs(s)
+        # LINK 1->2
+        T1_2 = Matrix([[            cos(q2),            -sin(q2),            0,              a1],
+                       [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+                       [sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+                       [                  0,                   0,            0,               1]])
+        T1_2 = T1_2.subs(s)
         
-        # LINK 3
-        T0_3 = Matrix([[            cos(q3),            -sin(q3),            0,              a2]
-                       [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d2]
-                       [sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d2]
-                       [                  0,                   0,            0,               1]
-        T0_3 = T0_3.subs(s)
+        # LINK 2->3
+        T2_3 = Matrix([[            cos(q3),            -sin(q3),            0,              a2],
+                       [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d2],
+                       [sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d2],
+                       [                  0,                   0,            0,               1]])
+        T2_3 = T2_3.subs(s)
         
-        # LINK 4 
-        T0_4 = Matrix([[            cos(q4),            -sin(q4),            0,              a3]
-                       [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4]
-                       [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4]
-                       [                  0,                   0,            0,               1]
-        T0_4 = T0_4.subs(s)
+        # LINK 3->4
+        T3_4 = Matrix([[            cos(q4),            -sin(q4),            0,              a3],
+                       [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+                       [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+                       [                  0,                   0,            0,               1]])
+        T3_4 = T3_4.subs(s)
         
-        # LINK 5
-        T0_5 = Matrix([[            cos(q5),            -sin(q5),            0,              a4]
-                       [sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5]
-                       [sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5]
-                       [                  0,                   0,            0,               1]
-        T0_5 = T0_5.subs(s)
+        # LINK 4->5
+        T4_5 = Matrix([[            cos(q5),            -sin(q5),            0,              a4],
+                       [sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+                       [sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+                       [                  0,                   0,            0,               1]])
+        T4_5 = T4_5.subs(s)
         
-        # LINK 6
-        T0_6 = Matrix([[            cos(q6),            -sin(q6),            0,              a5]
-                       [sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6]
-                       [sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6]
-                       [                  0,                   0,            0,               1]
-        T0_6 = T0_6.subs(s)
-        	#
+        # LINK 5->6 
+        T5_6 = Matrix([[            cos(q6),            -sin(q6),            0,              a5],
+                       [sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+                       [sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+                       [                  0,                   0,            0,               1]])
+        T5_6 = T5_6.subs(s)
+        
+        # LINK 6->G (Gripper)
+        T6_G = Matrix([[            cos(q7),            -sin(q7),            0,              a6],
+                       [sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha5)*d7],
+                       [sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha5)*d7],
+                       [                  0,                   0,            0,               1]])
+        T6_G = T6_G.subs(s)
+        
+        #
 	# Create individual transformation matrices
 	#
-	#
+	# Next we incrementally build the individual the total homogeneous transform from the BASELINK (0) to each link
+	T0_2 = simplify(T0_1 * T1_2) # base link to link 2
+	T0_3 = simplify(T0_2 * T2_3) # base link to link 3
+	T0_4 = simplify(T0_3 * T3_4)
+	T0_5 = simplify(T0_4 * T4_5)
+	T0_6 = simplify(T0_5 * T5_6)
+	T0_G = simplify(T0_6 * T6_G)
+	
+	### DEBUG output
+	print("T0_1 = ", T0_1.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+	print("T0_2 = ", T0_2.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+	print("T0_3 = ", T0_3.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+	print("T0_4 = ", T0_4.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+	print("T0_5 = ", T0_5.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+	print("T0_6 = ", T0_6.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+	print("T0_G = ", T0_G.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+					
 	# Extract rotation matrices from the transformation matrices
 	#
+	R_z = Matrix([[cos(np.pi), -sin(np.pi), 0, 0],
+	              [sin(np.pi),  cos(np.pi), 0, 0],
+	              [         0,           0, 1, 0],
+	              [         0,           0, 0, 1]])
+	R_y = Matrix([[ cos(np.pi), sin(np.pi), 0, 0],
+	              [         0,           1, 0, 0],
+	              [-sin(np.pi), cos(np.pi), 0, 0],
+	              [         0,           0, 0, 1]])
+        R_corr = simplify(R_z * R_y)
 	#
+	T_total = simplify(T0_G * R_corr)
         ###
 
         # Initialize service response
