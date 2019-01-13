14e691696dcd52ea2b3d2568d84371ed2e618255
Peter Hurt <peter.hurt@web.de>
2019-01-13 05:03:52 -0800

    Added the IK_server.py node to the launch configuration. Now one not has to start it all the time manually.

--- kuka_arm/scripts/safe_spawner.sh
+++ kuka_arm/scripts/safe_spawner.sh
@@ -6,5 +6,9 @@ x-terminal-emulator -e roslaunch kuka_arm cafe.launch &
 sleep 3 &&
 x-terminal-emulator -e roslaunch kuka_arm spawn_target.launch &
 sleep 5 &&
+x-terminal-emulator -e rosrun kuka_arm IK_server.py & 
+sleep 3 &&
 #x-terminal-emulator -e roslaunch kuka_arm inverse_kinematics.launch
 roslaunch kuka_arm inverse_kinematics.launch
+
+
